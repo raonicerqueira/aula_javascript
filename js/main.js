c0dcfc4a17285093f6ba2a84fe64d127eb032ab1
raonicerqueira <raoni_henrique@live.com>
2021-06-10 18:09:38 -0300

    aula de introdução ao javascript - bootcamp DIO

---
+++ js/main.js
@@ -0,0 +1,106 @@
+
+function clicou() {
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+//   alert("Obrigado por clicar")
+}
+
+function redirecionar() {
+    window.open("https://www.gamevicio.com/");  
+}
+
+
+
+
+/*
+function soma(n1, n2) {
+    return n1 + n2;    
+}
+
+var validar = 0;
+
+function validaIdade(idade) {
+    if (idade >= 18){
+        validar=true
+    }
+    else{
+        validar=false
+    }    
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+validaIdade(idade)
+console.log(validar)
+*/
+
+
+
+//alert(soma(5, 10));
+
+/*
+var d = new Date();
+alert(d.getDate());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+/*
+var count;
+for (count=0;  count <= 5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while (count <=5) {
+    console.log(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt ("Qual sua idade?");
+if (idade >= 18){
+    alert("Você é maior de idade");
+}
+else {
+    alert("Você é menor de idade");
+}
+*/
+
+
+/*
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(furta.cor);
+*/
+
+/*
+var lista = ["maça", "pêra", "laranja"]
+lista.push("uva");
+lista.pop();
+*/
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+//var nome = "Raoni Cerqueira";
+//var n1 = 29;
+//var n2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.replace("Japão", "Brasil"));
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
